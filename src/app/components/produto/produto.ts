30fe5fb0ed0ee8ba9aad306bbbf970f89042ade8
arthur da costa <arthurcosta2727@gmail.com>
2026-07-14 14:29:26 -0300

    adiciona componte pipe

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-
+import {UpperCasePipe, CurrencyPipe}from '@angular/common';
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe, CurrencyPipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
 export class Produto {
-  produto='notebook';
-  preco= 2500;
-  mostrarPreco= true;
-  mostrarproduto= false;
-
+ // produto='notebook';
+ // preco= 2500;
+ // mostrarPreco= true;
+ // mostrarproduto= false;
+produtos=[
+  {produto:'monitor',preco: 1000},
+  {produto:'teclado',preco:50.00},
+  {produto:'mause',preco: 26.00},
+  {produto:'desktop',proco: 1500},
+];
 }
