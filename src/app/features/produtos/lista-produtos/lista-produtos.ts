3847badbed76ee3ac6e86437d607c9183fa60547
alvaro_alves <alvaroalves264@gmail.com>
2026-07-16 15:51:39 -0300

    fear(lista-produtos):atualizou o modelo de produtos

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Produto } from '../produto/produto';
+import { Component,signal } from '@angular/core';
+import { Produto } from '../../../features/produtos/produto/produto';
 @Component({
   selector: 'app-lista-produtos',
  imports: [Produto],
@@ -7,14 +7,19 @@ import { Produto } from '../produto/produto';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-Produtos=[
+Produtos = signal ([
   {nome: 'teclado',preco:49.00},
   {nome: 'mause gamer',preco:299.99},
   {nome: 'monitor',preco:1599.99},
   {nome:'desktop',preco:49.00},
   {nome: 'headset gamer',preco:699.99},
-];
+]);
 exibirProduto(nome: string) {
   console.log('Produto selecionado:', nome);
+}
+adicionarProduto() {
+ this.Produtos.update((listaAtual) => [
+  ...listaAtual,{nome:'Sony Playstation 5',preco:10000}
+ ]);
 }
 }
