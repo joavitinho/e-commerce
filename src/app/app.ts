44c861de3317e5722101972bb5d34d4fc7a98715
alvaro_alves <alvaroalves264@gmail.com>
2026-07-20 16:58:15 -0300

    feat(app.ts): adicionou novas rota de navegaçao

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,17 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router'; //remova importaçao do RouterOutlet,pois nao esta sendo usado no momento
-import { Produto } from '../app/features/produtos/produto/produto';
-import { ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
+import { RouterOutlet, RouterLink } from '@angular/router'; //remova importaçao do RouterOutlet,pois nao esta sendo usado no momento
+//import { Produto } from '../app/features/produtos/produto/produto';
+//import { ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
+
+
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+nomeLoja = 'JV Studio'; //nome do e-commerce
+
 }
