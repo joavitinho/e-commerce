a31452d79342e33daa13f1a0cd6ab550f5f31783
alvaro_alves <alvaroalves264@gmail.com>
2026-07-21 16:33:25 -0300

    refactor(app)atualizamos app.toutes.ts e criamos a pagina home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,55 @@
-import { Routes } from '@angular/router';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
-import { Carrinho } from './features/carrinho/carrinho/carrinho';
+// // import { Routes } from '@angular/router';
+// // //import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+// // import { Carrinho } from './features/carrinho/carrinho/carrinho';
+// // import { Home } from './features/home/home/home';
 
-export const routes: Routes = [
+// // export const routes: Routes = [
+// // {
+// //     path: '',
+// //     component: Home,
+// // },
+// // // codigo Legado Roud
+// // //{
+// // //     path:'produtos',
+// // //     component: ListaProdutos,
+// // // },
+// // {
+// // path:'produtos',
+// // loadComponent: ()=>
+// //     import('./features/produtos/lista-produtos/lista-produtos').then(m =>.ListaProdutos)
+
+// // },
+
+// // {
+// //     path: 'Carrinho',
+// //     component: Carrinho
+// // },
+// // ];
+//! codigo Final LazyLoading e LoadComponent
+import { Routes } from "@angular/router";
+export const routes: Routes =[
 {
-    path: '',
-    component: ListaProdutos,
+    path:'',
+    loadComponent: () =>
+    import('./features/home/home/home').then((m)=>m.Home),
+
 },
 {
-    path:'carrinho',
-    component: Carrinho,
+path: 'produtos',
+loadComponent:() =>
+    import ('./features/produtos/lista-produtos/lista-produtos').then((m)=> m.ListaProdutos),
+
 },
+{
+   path:'carrinho',
+   loadComponent: () =>
+  import ('./features/carrinho/carrinho/carrinho').then((m) =>m.Carrinho),
+
+},
+{
+    path: '**',
+    redirectTo: '',
+},
+
+
 ];
