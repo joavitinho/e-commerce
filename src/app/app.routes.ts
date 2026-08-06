2b46f629c6329ba35e0dcaafc98bef9e82470666
jovana vieira miranda <jovanavieira802@gmail>
2026-08-06 16:37:25 -0300

    Modificamos o src/app/app.css no começo deu erro mais conseguimos resolver atualizamos tambem

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -49,9 +49,14 @@ loadComponent:() =>
 
 },
 {
+        path:'carrinho',
+        canActivate: [],
+        loadComponent: ()=>
+            import ('./features/checkout/checkout/checkout').then((m) =>m.Checkout),
+},
+{
     path: '**',
     redirectTo: '',
 },
-
 
 ]
